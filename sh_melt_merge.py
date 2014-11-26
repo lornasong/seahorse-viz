d558042d92a7d927853fab75a356c4edc389faab
lornasong <lsong@achievementnetwork.org>
2014-11-26 15:34:59 -0500

    Added line for merging two data sets.

--- sh_melt_merge.py
+++ sh_melt_merge.py
@@ -1,11 +1,17 @@
 import pandas as pd
 import numpy as np
 
-dive = pd.read_csv("C:\Users\lsong\Documents\GitHub", index_col = None,  header = 0,  low_memory = False,  na_values = '?')
-seahorse = pd.read_csv("C:\Users\lsong\Documents\GitHub", index_col = None,  header = 0,  low_memory = False,  na_values = '?')
+#Assumes dive and seahorse CSVs share SITE and DATE columns to join on.
+
+dive = pd.read_csv("C:\Users\lsong\Documents\GitHub\sh-data-format\dive_data.csv", index_col = None,  header = 0,  low_memory = False,  na_values = '')
+seahorse = pd.read_csv("C:\Users\lsong\Documents\GitHub\sh-data-format\seahorse_data.csv", index_col = None,  header = 0,  low_memory = False,  na_values = '')
 
 environment = ['PENCIL_URCHIN', 'HYDROZOAN', 'SOFT_CORAL', 'ANEMONE', 'SPONGE_SOL', 'SPONGE_SUB', 'SEAPEN', 'SEAGRASS', 'SHELL']
 
-melt = pd.melt(dive, id_vars = ['DATE',  'SITE', 'LONGITUDE',  'LATITUDE', 'DEPTH', 'SST', 'BT', 'VISIBILITY', 'SEAHORSES'], value_vars = environment)
+merge = pd.merge(dive,  seahorse,  how = "left",  on = ["SITE",  "DATE"])
 
-melt.to_csv(r'C:\Users\lsong\Desktop\melt_dive_sh.csv')
+#merge.to_csv(r'C:\Users\lsong\Desktop\mere.csv')
+
+melt = pd.melt(merge, id_vars = ['DATE',  'SITE', 'LONGITUDE',  'LATITUDE', 'DEPTH', 'SST', 'BT', 'VISIBILITY', 'SEAHORSES'], value_vars = environment)
+
+#melt.to_csv(r'C:\Users\lsong\Desktop\melt_dive_sh.csv')
